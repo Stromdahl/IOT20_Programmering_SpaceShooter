d73fd21bbf6198d426e8360b10c5f9da02dd6b0f
mattias <mattias.stromdahl@gmail.com>
2020-10-15 15:53:38 +0200

    Added a player class

---
+++ src/com/company/gameObjects/Player.java
@@ -0,0 +1,18 @@
+package com.company.gameObjects;
+
+import java.awt.*;
+
+public class Player extends GameObject{
+
+    Player(double x, double y) {
+        super(x, y);
+    }
+
+    public void update() {
+
+    }
+
+    public void display(Graphics graphics) {
+
+    }
+}
